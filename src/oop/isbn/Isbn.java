27a0be1a79b69f4497fbc754f02a7af160e79c23
Oliver Wanke <oliver.wanke@acpp.de>
2025-02-17 01:25:13 +0100

    methoden jetzt static

--- src/oop/isbn/Isbn.java
+++ src/oop/isbn/Isbn.java
@@ -1,42 +1,38 @@
 package oop.isbn;
 
-
-
 public class Isbn
 {
-       
+
     public Isbn()
     {
-       
+
     }
-    
-    public String generateIsbn10(String input)
+
+    public static String generateIsbn10(String input)
     {
-      
-        
-        
-//      remove bindestrich
-        String prefix = input.replaceAll("\\D+","");
-        
-        System.out.println("input: " +input);
-        System.out.println("prefix: " +prefix);
+
+        // remove bindestrich
+        String prefix = input.replaceAll("\\D+", "");
+
+        System.out.println("input: " + input);
+        System.out.println("prefix: " + prefix);
         // check for length
         if (prefix.length() != 9)
         {
             throw new IllegalArgumentException();
         }
-        
-//      gewichtete Summe berechnen
-        int summe=0;
-        for(int i=0;i<9;i++)
+
+        // gewichtete Summe berechnen
+        int summe = 0;
+        for (int i = 0; i < 9; i++)
         {
-            int digit=Integer.parseInt(prefix.substring(i, i+1));
-            summe = summe + (10-i)*digit;
+            int digit = Integer.parseInt(prefix.substring(i, i + 1));
+            summe = summe + (10 - i) * digit;
         }
-        
-//      pruefziffer berechnen   
-        int pruefziffer = (11-(summe % 11)) % 11;
-        
+
+        // pruefziffer berechnen
+        int pruefziffer = (11 - (summe % 11)) % 11;
+
         String suffix;
         if (pruefziffer == 10)
         {
@@ -46,43 +42,41 @@ public class Isbn
         {
             suffix = Integer.toString(pruefziffer);
         }
-        
+
         return input + "-" + suffix;
     }
-    
-    public String generateIsbn13(String prefix)
+
+    public static String generateIsbn13(String prefix)
     {
-        
-//        remove bindestrich
-        prefix = prefix.replaceAll("\\D+","");
-        
-//      check for length
+
+        // remove bindestrich
+        prefix = prefix.replaceAll("\\D+", "");
+
+        // check for length
         if (prefix.length() != 12)
         {
             throw new IllegalArgumentException();
         }
-        int summe1=0;
-        int summe2=0;
-        for (int i=0;i<12;i++)
+        int summe1 = 0;
+        int summe2 = 0;
+        for (int i = 0; i < 12; i++)
         {
-            int digit=Integer.parseInt(prefix.substring(i, i+1));
-            if ((i+1)%2 !=0)
+            int digit = Integer.parseInt(prefix.substring(i, i + 1));
+            if ((i + 1) % 2 != 0)
             {
-                summe1=summe1 + digit;
+                summe1 = summe1 + digit;
             }
             else
             {
-                summe2=summe2 + digit;
+                summe2 = summe2 + digit;
             }
-            
+
         }
-        int summe = 3*summe2 + summe1;
-        int pruefziffer=(10-summe%10)%10;
+        int summe = 3 * summe2 + summe1;
+        int pruefziffer = (10 - summe % 10) % 10;
         String suffix = Integer.toString(pruefziffer);
-        
-        
+
         return prefix + "-" + suffix;
     }
-    
 
 }
